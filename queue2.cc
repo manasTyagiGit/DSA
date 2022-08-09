20527d995f8da01cf679245ad803c0e049a43fb6
manasTyagiGit <manastyagi.ind@gmail.com>
2022-08-09 09:13:04 +0530

    Correction on line 40 : '*rear == NULL' corrected to '*rear = NULL'

--- queue2.cc
+++ queue2.cc
@@ -37,7 +37,7 @@ bool dequeue (queue** rear, queue** front)
 
         if (*front == NULL)             // only one node is present
         {              
-            *rear == NULL;
+            *rear = NULL;
         }
 
         int valDequeued = deletedNode -> value;
@@ -97,72 +97,69 @@ int main (void)
 
     int choice = -1;
 
-    redo :
-
-    printf ("\nPlease enter your choice regarding what operation you want to perform\n");
-    printf ("1. Enqueue a value into the queue rear\n");
-    printf ("2. Dequeue a value from the front of the queue\n");
-    printf ("3. Print the rear value of the queue\n");
-    printf ("4. Print the front value of the queue\n");
-    printf ("5. Print the entire queue\n");
-    printf ("Your choice :: ");
-    scanf ("%d", &choice);
-
-    switch (choice)
+    int chc = 1;
+    while (chc == 1)
     {
-        case 1 :
+        printf ("\nPlease enter your choice regarding what operation you want to perform\n");
+        printf ("1. Enqueue a value into the queue rear\n");
+        printf ("2. Dequeue a value from the front of the queue\n");
+        printf ("3. Print the rear value of the queue\n");
+        printf ("4. Print the front value of the queue\n");
+        printf ("5. Print the entire queue\n");
+        printf ("Your choice :: ");
+        scanf ("%d", &choice);
+
+        switch (choice)
         {
-            int key = 0;
-            printf ("\nEnter the value of the key you want to enqueue :: ");
-            scanf("%d", &key);
+            case 1 :
+            {
+                int key = 0;
+                printf ("\nEnter the value of the key you want to enqueue :: ");
+                scanf("%d", &key);
 
-            if ((enqueue (&rear, &front, key)))         printf ("\nSUCCESS! Value = %d has been enqueued\n", key);
-            else                                        printf ("\nFAILURE\n");
+                if ((enqueue (&rear, &front, key)))         printf ("\nSUCCESS! Value = %d has been enqueued\n", key);
+                else                                        printf ("\nFAILURE\n");
 
-            break;
+                break;
+            }
+
+            case 2 :
+            {
+                if (dequeue (&rear, &front))               printf ("\nSUCCESS\n");
+                else                                       printf ("\nFAILURE. QUEUE might be empty\n");
+                break;
+            }
+
+            case 3 :
+            {
+                if (!peekRear (rear))                      printf ("\nFAILURE. QUEUE might be empty\n");
+                break;               
+            }
+
+            case 4 :
+            {
+                if (!peekFront (front))                      printf ("\nFAILURE. QUEUE might be empty\n");
+                break;
+            }
+
+            case 5 :
+            {
+                printf ("\nThe queue is :: \n");
+                printQueue (rear, front);
+                break;
+            }
+
+            default :
+            {
+                printf ("\nWRONG CHOICE! ERROR!\n");
+                break;
+            }
         }
+        printf("\n\n");
 
-        case 2 :
-        {
-            if (dequeue (&rear, &front))               printf ("\nSUCCESS\n");
-            else                                       printf ("\nFAILURE. QUEUE might be empty\n");
-            break;
-        }
-
-        case 3 :
-        {
-            if (!peekRear (rear))                      printf ("\nFAILURE. QUEUE might be empty\n");
-            break;               
-        }
-
-        case 4 :
-        {
-            if (!peekFront (front))                      printf ("\nFAILURE. QUEUE might be empty\n");
-            break;
-        }
-
-        case 5 :
-        {
-            printf ("\nThe queue is :: \n");
-            printQueue (rear, front);
-            break;
-        }
-
-        default :
-        {
-            printf ("\nWRONG CHOICE! ERROR!\n");
-            break;
-        }
+        printf ("\nDo you want to continue (1) or exit (0) :: ");
+        scanf ("%d", &chc);
     }
-    printf("\n\n");
-
-    printf ("\nDo you want to continue (1) or exit (0) :: ");
-    scanf ("%d", &choice);
-
-    if (choice == 1)
-    {
-        goto redo;
-    }   
 
     return EXIT_SUCCESS;
 }
