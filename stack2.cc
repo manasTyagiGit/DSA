3873b2acecdafbaf78f59a86d0e1c42a676991ee
manasTyagiGit <manastyagi.ind@gmail.com>
2022-08-09 08:52:38 +0530

    Remove dangerous 'goto' statement from stack1.cc

--- stack2.cc
+++ stack2.cc
@@ -63,66 +63,66 @@ int main (void)
     int choice = -1;
     stack* top = NULL;
 
-    redo :   
+    int chc = 1;
 
-    printf ("\nPlease enter your choice regarding what operation you want to perform\n");
-    printf ("1. Push a value onto the stack\n");
-    printf ("2. Pop a value from the top of the stack\n");
-    printf ("3. Peek the top value of stack\n");
-    printf ("4. Print the stack\n");
-    printf ("Your choice :: ");
-    scanf ("%d", &choice);
+    while(chc == 1)
+    {  
 
-    switch (choice)
-    {
-        case 1 :
+        printf ("\nPlease enter your choice regarding what operation you want to perform\n");
+        printf ("1. Push a value onto the stack\n");
+        printf ("2. Pop a value from the top of the stack\n");
+        printf ("3. Peek the top value of stack\n");
+        printf ("4. Print the stack\n");
+        printf ("Your choice :: ");
+        scanf ("%d", &choice);
+
+        switch (choice)
         {
-            int key = 0;
-            printf ("\nEnter the value you want to push onto the stack top :: ");
-            scanf("%d", &key);
+            case 1 :
+            {
+                int key = 0;
+                printf ("\nEnter the value you want to push onto the stack top :: ");
+                scanf("%d", &key);
 
-            push (&top, key);
-            printf ("\nValue = %d pushed onto the top of the stack.", key);
-            break;
-        }
+                push (&top, key);
+                printf ("\nValue = %d pushed onto the top of the stack.", key);
+                break;
+            }
 
-        case 2 :
-        {
-            int valPopped = pop (&top);
-            (valPopped >= 0) ? (printf ("\nValue = %d popped", valPopped)) : (printf ("\nStack is empty! Error!\n"));
-            break;
-        }
+            case 2 :
+            {
+                int valPopped = pop (&top);
+                (valPopped >= 0) ? (printf ("\nValue = %d popped", valPopped)) : (printf ("\nStack is empty! Error!\n"));
+                break;
+            }
 
-        case 3 :
-        {
-            int valTop = peek (top);
-            (valTop >= 0) ? (printf ("\nValue of the top = %d", valTop)) : (printf ("\nStack is empty! Error!\n"));
-            break;
-        }
+            case 3 :
+            {
+                int valTop = peek (top);
+                (valTop >= 0) ? (printf ("\nValue of the top = %d", valTop)) : (printf ("\nStack is empty! Error!\n"));
+                break;
+            }
 
-        case 4:
-        {
-            printf ("\nThe stack is :: \n");
-            printStack (top);
-            break;
-        }
+            case 4:
+            {
+                printf ("\nThe stack is :: \n");
+                printStack (top);
+                break;
+            }
 
-        default :
-        {
-            printf ("\nNot defined. ERROR!\n");
-            break;
+            default :
+            {
+                printf ("\nNot defined. ERROR!\n");
+                break;
+            }
         }
+    
+
+        printf("\n\n");
+
+        printf ("\nDo you want to continue (1) or exit (0) :: ");
+        scanf ("%d", &chc);
     }
-
-    printf("\n\n");
-
-    printf ("\nDo you want to continue (1) or exit (0) :: ");
-    scanf ("%d", &choice);
-
-    if (choice == 1)
-    {
-        goto redo;
-    }   
 
     return EXIT_SUCCESS;
 }
