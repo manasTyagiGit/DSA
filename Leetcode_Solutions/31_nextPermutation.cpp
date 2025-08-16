f12ba889a488e6cd8cad66e202f68c5d11c77b1c
manasTyagiGit <manastyagi.ind@gmail.com>
2025-08-16 11:40:17 +0530

    add comments

--- Leetcode_Solutions/31_nextPermutation.cpp
+++ Leetcode_Solutions/31_nextPermutation.cpp
@@ -11,12 +11,21 @@ public:
             the array, i.e., the end of the array, and then 
             swap those, and finally sort between the breakpoint,
             and final.
+
+            We need to match the longest prefix possible, hence
+            taking the breakpoint from the right, and not left
+
+            Once we swap, just sort the portion after the breakpoint
+            as that is the smallest string lexicographically.
+
+            If no breakpoint, it is the largest string in the lexicon,
+            hence return a sorted (or cleverly, the reverse) of the string
         */
 
         int sz = nums.size();
         int breakpoint = -1;
 
-        for (int i = sz - 2; i >= 0; i--)
+        for (int i = sz - 2; i >= 0; i--)               // first breakpoint from the right
         {
             if (nums[i] < nums[i + 1])
             {
@@ -25,13 +34,13 @@ public:
             }
         }
 
-        if (breakpoint == -1)
+        if (breakpoint == -1)                           // in case the largest string in lexicon
         {
             reverse (nums.begin(), nums.end());
             return;
         }
         
-        for (int i = sz - 1; i > breakpoint; i--)
+        for (int i = sz - 1; i > breakpoint; i--)       // find the first larger element than the breakpoint from right, and swap
         {
             if (nums[breakpoint] < nums[i])
             {
@@ -40,7 +49,7 @@ public:
             }
         }
 
-        reverse (nums.begin() + breakpoint + 1, nums.end()) ;      
+        reverse (nums.begin() + breakpoint + 1, nums.end()) ;       // clever sort way, can also use sort())
 
     }
 };
