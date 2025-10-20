507ec55112566943a9f73395873cd0682d2da6dd
manasTyagiGit <manastyagi.ind@gmail.com>
2025-10-20 22:26:43 +0530

    Add LC33

---
+++ Leetcode_Solutions/33_searchinRotatedArray.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int>& nums, int target) {
+    int high = nums.size() - 1;
+    int low = 0;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target)            return mid;
+
+        if (nums[low] <= nums[mid])         // branch 1
+        {   
+            if (target >= nums[low] && target < nums[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;                
+        }
+
+        else                                // branch 2
+        {
+            if (target > nums[mid] && target <= nums[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }            
+    }
+
+    return -1;    
+}
